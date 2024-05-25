f5b13ec09f4cfc1531098f12a7c139fe6c813330
ANIRUDDHA BISWAS <aniruddhaa.biswas@gmail.com>
2024-05-25 21:55:28 +0530

    file upadted

--- Day_6/my_dict2.py
+++ Day_6/my_dict2.py
@@ -1,6 +1,13 @@
 math_marks = {'Rifat':85, 'Siam': 71, 'Meghla':90, 'Shamim':82}
-max_value = max(math_marks.values())
-
+# max_value = max(math_marks.values())
 # print(max_value)
+
+max_key = next(iter(math_marks))
+
+for key in math_marks:
+    if math_marks[key]>math_marks[max_key]:
+        max_key = key
+print(max_key)
+
 
 
